0f08e3c2106af70fd6d6b6925fc84953a03d055c
Yohan <yohan@yohanpro.com>
2020-08-17 11:19:19 +0900

    1. react-icons install, icon 적용 2. ListCard에 Task, due 레이아웃 배치

--- src/ListCard/index.tsx
+++ src/ListCard/index.tsx
@@ -1,27 +1,34 @@
 import React from "react";
-import {
-  Container,
-  Row,
-  Col,
-  CardTitle,
-  Card,
-  CardBody,
-  CardSubtitle,
-  CardText,
-  Button,
-} from "reactstrap";
+import { Card, CardBody } from "reactstrap";
+import { AiFillEdit } from "react-icons/ai";
+import { BsTrash } from "react-icons/bs";
 
 const ListCard: React.FC = () => {
   return (
     <Card>
       <CardBody>
-        <CardTitle>Card title</CardTitle>
-        <CardSubtitle>Card subtitle</CardSubtitle>
-        <CardText>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </CardText>
-        <Button className="btn-block">Button</Button>
+        <div className="list__main">
+          <div className="list-task">
+            <div>Task</div>
+            <span className="list-task__subTitle">dkfjdkf</span>
+          </div>
+          <div className="list-due mt-4">
+            <div>due</div>
+            <span className="list-task__dueDate">2020.8.17</span>
+          </div>
+        </div>
+        <div className="list__iconArea">
+          <AiFillEdit
+            className="list-icon list__edit"
+            size="25"
+            color="rgba(113,113,113,1)"
+          />
+          <BsTrash
+            className="list-icon list__delete"
+            size="25"
+            color="rgba(113,113,113,1)"
+          />
+        </div>
       </CardBody>
     </Card>
   );
--- src/components/AddTask/index.tsx
+++ src/components/AddTask/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import ListCard from "src/ListCard";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const AddTask: React.FC = () => {
   return (
     <Container className="container__addTask">
       <Row className="mt-5 align-items-center">
         <Col xs="4">
-          <span>+ </span> New Task
+          <span>
+            <AiOutlinePlus size={23} />
+          </span>{" "}
+          <span>New Task</span>
         </Col>
       </Row>
       <Row>
