8fd5737c2de748e314528a1472c02f2d8fa73340
Yohan Kim <yohan.kim@innoboost.co.kr>
2020-09-01 17:09:52 +0900

    1. Modal 레이아웃 배치 2. icomoon으로 svg 삽입. react-icomoon

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { Route, BrowserRouter } from "react-router-dom";
-import Login from "src/pages/login";
-import Home from "src/pages/home";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/index.scss";
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Login from 'src/pages/login';
+import Home from 'src/pages/home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/home">
-          <Home />
-        </Route>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route path="/home">
+					<Home />
+				</Route>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- src/ListCard/index.tsx
+++ src/ListCard/index.tsx
@@ -1,63 +1,56 @@
-import React, { useState } from "react";
-import { Card, CardBody } from "reactstrap";
-import { AiFillEdit } from "react-icons/ai";
-import { BsTrash } from "react-icons/bs";
-import EditModal from "src/components/Modal";
+import React, { useState } from 'react';
+import { Card, CardBody } from 'reactstrap';
+import { AiFillEdit } from 'react-icons/ai';
+import { BsTrash } from 'react-icons/bs';
+import EditModal from 'src/components/Modal';
 
 interface CardProps {
-  id: number;
-  title: string;
-  completed: boolean;
-  userId: number;
+	id: number;
+	title: string;
+	completed: boolean;
+	userId: number;
 }
 
 const ListCard: React.FC<CardProps> = (props: CardProps) => {
-  const { id, title, completed, userId } = props;
+	const { id, title, completed, userId } = props;
 
-  const [editModal, setEditModal] = useState(false);
+	const [
+		editModal,
+		setEditModal
+	] = useState(false);
 
-  const toggle = (targetModal: string) => {
-    switch (targetModal) {
-      case "Edit":
-        setEditModal(!editModal);
-        break;
+	const toggle = (targetModal: string) => {
+		switch (targetModal) {
+			case 'Edit':
+				setEditModal(!editModal);
+				break;
 
-      default:
-        break;
-    }
-  };
+			default:
+				break;
+		}
+	};
 
-  return (
-    <Card>
-      <CardBody>
-        <div className="list__main" onClick={() => toggle("Edit")}>
-          <div className="list-task">
-            <div>Task</div>
-            <span className="list-task__subTitle">{title}</span>
-          </div>
-          <div className="list-due mt-4">
-            <div>Completed</div>
-            <span className="list-task__dueDate">
-              {completed ? "Yes" : "Not Yet"}
-            </span>
-          </div>
-        </div>
-        <div className="list__iconArea">
-          <AiFillEdit
-            className="list-icon list__edit"
-            size="25"
-            color="rgba(113,113,113,1)"
-          />
-          <BsTrash
-            className="list-icon list__delete"
-            size="25"
-            color="rgba(113,113,113,1)"
-          />
-        </div>
-      </CardBody>
-      <EditModal modal={editModal} toggle={toggle} {...props} />
-    </Card>
-  );
+	return (
+		<Card>
+			<CardBody>
+				<div className="list__main" onClick={() => toggle('Edit')}>
+					<div className="list-task">
+						<div>Task</div>
+						<span className="list-task__subTitle">{title}</span>
+					</div>
+					<div className="list-due mt-4">
+						<div>Completed</div>
+						<span className="list-task__dueDate">{completed ? 'Yes' : 'No'}</span>
+					</div>
+				</div>
+				<div className="list__iconArea">
+					<AiFillEdit className="list-icon list__edit" size="25" color="rgba(113,113,113,1)" />
+					<BsTrash className="list-icon list__delete" size="25" color="rgba(113,113,113,1)" />
+				</div>
+			</CardBody>
+			<EditModal modal={editModal} toggle={toggle} {...props} />
+		</Card>
+	);
 };
 
 export default ListCard;
