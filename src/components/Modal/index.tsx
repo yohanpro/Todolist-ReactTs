b43ab347456544e2abab74f7913e31d3b604bfc6
Yohan <yohan@yohanpro.com>
2020-09-20 11:57:19 +0900

    1.recoil js 설치 2. NestedModal 작성

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -8,8 +8,12 @@ import {
   ModalFooter,
   Form,
   FormGroup,
+  ModalHeader,
 } from "reactstrap";
+import { atom, selector, useRecoilState, useRecoilValue } from "recoil";
+
 import Icon from "../Icon";
+import ConfirmModal from "./confirmModal";
 
 interface ModalProps {
   modal: boolean;
@@ -23,30 +27,47 @@ const EditModal = (props: ModalProps) => {
   const { modal, toggle } = props;
   const { completed, title } = props;
 
+  const [isEdited, setIsEdited] = useState(false);
+  const [nestedModal, setNestedModal] = useState(false);
+
+  const nestedModalState = atom({
+    key: "myTextstate", // unique ID (with respect to other atoms/selectors)
+    default: "", // default value (aka initial value)
+  });
+
+  const [text, setText] = useRecoilState(nestedModalState);
+
   const taskInput = useRef<HTMLInputElement>(null);
   const completed_1_Input = useRef<HTMLInputElement>(null);
   const completed_2_Input = useRef<HTMLInputElement>(null);
 
-  const EditAction = () => {
+  const toggleNested = (action: string) => {
+    if (isEdited || action === "delete") {
+      setNestedModal(!nestedModal);
+    } else {
+      toggle("Edit");
+    }
+  };
+
+  const EditAction = (action: any) => {
     if (taskInput.current) {
       taskInput.current.disabled = false;
     }
 
     if (completed_1_Input.current && completed_2_Input.current) {
-      console.log(completed_1_Input);
       completed_1_Input.current.disabled = false;
       completed_2_Input.current.disabled = false;
-
-      // completed_1_Input.current.checked = "false";
-      // completed_2_Input.current.checked = "false";
     }
+
+    setText(action);
+    setIsEdited(true);
   };
 
   return (
     <div>
       <Modal isOpen={modal} toggle={() => toggle("")}>
-        {/* <Modal isOpen={modal} toggle={toggle} className={className}> */}
         <ModalBody>
+          <ConfirmModal nestedModal={nestedModal} toggleNested={toggleNested} />
           <Form className="Todo__Edit">
             <FormGroup disabled>
               <Label>Task</Label>
@@ -92,15 +113,21 @@ const EditModal = (props: ModalProps) => {
           </Form>
         </ModalBody>
         <ModalFooter>
-          <Button className="btn btn_modal" onClick={EditAction}>
+          <Button className="btn btn_modal" onClick={() => EditAction("edit")}>
             <Icon color="#607d8b" className="mr-1" size={25} icon="pencil" />
             EDIT
           </Button>{" "}
-          <Button className="btn btn_modal" onClick={() => toggle("")}>
+          <Button
+            className="btn btn_modal"
+            onClick={() => toggleNested("delete")}
+          >
             <Icon color="#607d8b" className="mr-1" size={25} icon="bin" />
             DELETE
           </Button>
-          <Button className="btn btn_modal" onClick={() => toggle("Edit")}>
+          <Button
+            className="btn btn_modal"
+            onClick={() => toggleNested("closed")}
+          >
             <Icon
               color="#607d8b"
               className="mr-1"
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,21 +1,18 @@
-import React, { useState } from 'react';
-import Header from 'src/components/header';
-import AddTask from 'src/components/AddTask';
+import React, { useState } from "react";
+import Header from "src/components/header";
+import AddTask from "src/components/AddTask";
 
 const Home: React.FC = () => {
-	const [
-		isLogin,
-		setIsLogin
-	] = useState(false);
+  const [isLogin] = useState(false);
 
-	return (
-		<div className="home">
-			<Header isLogin={isLogin} />
-			<main>
-				<AddTask />
-			</main>
-		</div>
-	);
+  return (
+    <div className="home">
+      <Header isLogin={isLogin} />
+      <main>
+        <AddTask />
+      </main>
+    </div>
+  );
 };
 
 export default Home;
